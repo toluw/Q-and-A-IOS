16eabfcc6c0e6ac1e488b3533f6a4f45a781c8d5
gigl-pc <oke.toluwase@giglogistics.com>
2026-01-22 09:07:36 +0100

    Initial Commit

---
+++ Q and A/Q_and_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_and_AApp.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Q_and_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
