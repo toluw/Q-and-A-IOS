f72706e10426147b8e0673931f9ead5f5a04998e
toluw <oketoluwase@gmail.com>
2026-01-28 15:43:21 +0100

    Add AppNavigator

---
+++ Q and A/App/Models/AppFlow.swift
@@ -0,0 +1,8 @@
+//
+//  AppFlow.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 28/01/2026.
+//
+
+import Foundation
---
+++ Q and A/App/Views/AppNavigator.swift
@@ -0,0 +1,18 @@
+//
+//  AppNavigator.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 28/01/2026.
+//
+
+import SwiftUI
+
+struct AppNavigator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppNavigator()
+}
