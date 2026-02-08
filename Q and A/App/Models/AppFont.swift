32d4682b9646887ee12a3a1fb52bbf070e15313e
toluw <oketoluwase@gmail.com>
2026-02-08 10:28:50 +0100

    Add Fonts

---
+++ Q and A/App/Models/AppFont.swift
@@ -0,0 +1,8 @@
+//
+//  AppFont.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 08/02/2026.
+//
+
+import Foundation
---
+++ Q and A/App/Views/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 30/01/2026.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
