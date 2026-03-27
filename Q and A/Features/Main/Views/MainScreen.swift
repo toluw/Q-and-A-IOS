6c75e3c25d52a714bb3b8907dc8bd8c715846ff6
toluw <oketoluwase@gmail.com>
2026-03-27 12:41:32 +0100

    Add Nav Drawer

--- Q and A/Features/Main/Views/MainScreen.swift
+++ Q and A/Features/Main/Views/MainScreen.swift
@@ -11,8 +11,45 @@ struct MainScreen: View {
     
     
     @State private var selectedTab: MainTab = .cbt
+    @State private var isDrawerOpen = false
     
     var body: some View {
+        ZStack {
+                   
+            // Main Content
+            mainContent
+                .disabled(isDrawerOpen)
+                .blur(radius: isDrawerOpen ? 3 : 0)
+            
+            
+            // Overlay
+             if isDrawerOpen {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                            closeDrawer()
+                        }
+                        .transition(.opacity)
+                }
+            
+           
+            // Drawer
+            HStack {
+                NavDrawer {
+                    closeDrawer()
+                    
+                }
+                .frame(width: 300)
+                .offset(x: isDrawerOpen ? 0 : -320)
+                            
+                Spacer()
+                }
+            
+        }.animation(.easeInOut(duration: 0.3), value: isDrawerOpen)
+    }
+    
+    @ViewBuilder
+    private var mainContent: some View{
         ZStack {
                    
                    // Current Screen
@@ -44,6 +81,16 @@ struct MainScreen: View {
                MainLibraryScreen()
            }
        }
+    
+    
+    private func openDrawer() {
+            isDrawerOpen = true
+    }
+        
+    private func closeDrawer() {
+            isDrawerOpen = false
+    }
+    
 }
 
 #Preview {
---
+++ Q and A/Features/Main/Views/NavDrawer.swift
@@ -0,0 +1,25 @@
+//
+//  NavDrawer.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 27/03/2026.
+//
+
+import SwiftUI
+
+struct NavDrawer: View {
+    
+    var onClose: () -> Void
+    
+    var body: some View {
+        VStack(){
+            Text("Nav Drawer")
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    NavDrawer(){}
+}
