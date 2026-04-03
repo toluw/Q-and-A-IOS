0f2fb0baff466f7b3d86d1e30e7969aa787856ca
toluw <oketoluwase@gmail.com>
2026-04-03 09:45:45 +0100

    Add Device manager

--- Q and A/Core/Utility/DeviceManager.swift
+++ Q and A/Core/Utility/DeviceManager.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+
+class DeviceManager {
+    
+    static let shared = DeviceManager()
+    
+    private let key = "device_id"
+    
+    func getDeviceId() -> String {
+        if let saved = KeychainHelper.get(key: key) {
+            return saved
+        } else {
+            let newId = UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString
+            KeychainHelper.save(key: key, value: newId)
+            return newId
+        }
+    }
+}
--- Q and A/Core/Utility/KeychainHelper.swift
+++ Q and A/Core/Utility/KeychainHelper.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class KeychainHelper {
+    
+    static func save(key: String, value: String) {
+        let data = value.data(using: .utf8)!
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+    
+    static func get(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        
+        if SecItemCopyMatching(query as CFDictionary, &dataTypeRef) == noErr {
+            let data = dataTypeRef as! Data
+            return String(data: data, encoding: .utf8)
+        }
+        
+        return nil
+    }
+}
