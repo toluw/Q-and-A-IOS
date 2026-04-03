a9167de9f9dc46ced599be931d436bfe24c3b7a6
toluw <oketoluwase@gmail.com>
2026-04-03 09:44:20 +0100

    Add Device manager

---
+++ Q and A/Core/Utility/DeviceManager.swift
@@ -0,0 +1,8 @@
+//
+//  DeviceManager.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 03/04/2026.
+//
+
+import Foundation
---
+++ Q and A/Core/Utility/KeychainHelper.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainHelper.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 03/04/2026.
+//
+
+import Foundation
