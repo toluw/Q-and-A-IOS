59ff7643bd1f5bc760b0aeabb656715720da5f51
toluw <oketoluwase@gmail.com>
2026-06-12 14:26:24 +0100

    Navigate to passage

---
+++ Q and A/Features/Cbt/View/PassageScreen.swift
@@ -0,0 +1,18 @@
+//
+//  PassageScreen.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 12/06/2026.
+//
+
+import SwiftUI
+
+struct PassageScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PassageScreen()
+}
