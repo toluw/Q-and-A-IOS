4aae4b43a3012b5eb5c769871a89d37d77943380
toluw <oketoluwase@gmail.com>
2026-07-01 09:58:39 +0100

    Multiple Exam Screen

--- Q and A/Features/Cbt/View/ExamPracticeScreen.swift
+++ Q and A/Features/Cbt/View/ExamPracticeScreen.swift
@@ -11,9 +11,55 @@ struct ExamPracticeScreen: View {
     
     @ObservedObject var navVm: MainNavViewModel
     @ObservedObject var cbtViewModel: CbtViewModel
+    @State private var selectedIndex: Int = 0
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            
+            HStack{
+              
+                Button {
+                    
+                    showErrorMessage(message: "Are you sure you want to exit?", actionTitle: "Exit", showCancel: true, action: {
+                        navVm.pop()
+                    })
+                   
+                } label: {
+                    
+                    Image(systemName: "xmark")
+                        
+                }.buttonStyle(.plain)
+                
+                
+                Spacer()
+               
+                
+                }.frame(maxWidth: .infinity)
+                .padding(.leading, 16)
+                .padding(.trailing, 16)
+                .padding(.top, 16)
+            
+            if(cbtViewModel.multipleExams.count > 1){
+                
+                CustomTabBar(
+                    tabs: cbtViewModel.multipleExams.map { $0.item },
+                    selectedIndex: $selectedIndex
+                ).padding(.top, 17)
+                
+            }
+            
+            if(!cbtViewModel.multipleExams.isEmpty){
+                PracticeScreen(multipleExam: cbtViewModel.multipleExams[selectedIndex], navVm: navVm)
+            }
+            
+            Spacer()
+            
+            
+            
+            
+            
+        }
     }
 }
 
