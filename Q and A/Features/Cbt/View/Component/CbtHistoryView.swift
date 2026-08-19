35e8a2c6baf1e203c91f519a5da4afbff0f529a5
toluw <oketoluwase@gmail.com>
2026-08-19 13:51:55 +0100

    Implement CBT History View

---
+++ Q and A/Features/Cbt/View/Component/CbtHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CbtHistoryView.swift
+//  Q and A
+//
+//  Created by GIGL-PC on 19/08/2026.
+//
+
+import SwiftUI
+
+struct CbtHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CbtHistoryView()
+}
